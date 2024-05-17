f9397cf3b63b524baaeccc61340c374db90bc96f
abhisheksarkar30 <8725485+abhisheksarkar30@users.noreply.github.com>
2024-05-17 16:53:28 +0530

    Refresh log added, test phase fix, environment profile added

--- limits-service/src/test/java/org/edu/abhi/limitsservice/LimitsServiceApplicationTests.java
+++ limits-service/src/test/java/org/edu/abhi/limitsservice/LimitsServiceApplicationTests.java
@@ -2,8 +2,10 @@ package org.edu.abhi.limitsservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("junit")
 class LimitsServiceApplicationTests {
 
 	@Test
